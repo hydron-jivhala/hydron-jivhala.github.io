90babc59ef2dbb0a0aedaf506a024229dfd1d934
Chetan Shinde <14790035+shindecs21@users.noreply.github.com>
2024-12-28 02:43:34 +0530

    fix for wow js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
   import * as main from "../lib/main.js";
   import Hero from "./Hero.svelte";
   import { selectedItem, medList } from "./data.svelte.js";
+  import scriptSrc2 from "wowjs/dist/wow.js?url";
 
   const startMed = medList.filter((m) => m.Item < 5);
-  let wow;
   onMount(() => {
     main.setupSwiper();
 
@@ -17,11 +17,16 @@
       selectedItem.item = card.getAttribute("data-bs-item"); // Extract info from data-bs-* attributes
     });
 
-
-    wow = new WOW({ live: false, animateClass: "animate__animated" }).init();
-
+    new WOW({ live: false, animateClass: "animate__animated" }).init();
   });
+
+
+
 </script>
+
+<svelte:head>
+  <script src={scriptSrc2}></script>
+</svelte:head>
 
 <Hero></Hero>
 
@@ -122,252 +127,5 @@
         </div>
       </div>
     {/each}
-  </div>
-
-  <div class="row mt-4 g-4" style="display: none;">
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div
-          class="card-body"
-          style="background: linear-gradient(157.59deg, hsla(0, 0%, 100%, 0.1) -2.85%, rgba(87, 67, 143, 0.1) 89.86%)"
-        >
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a
-              class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-              data-bs-toggle="modal"
-              data-bs-target="#productModal"
-            >
-              <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                <i class="fa fa-arrow-right"></i>
-              </span>
-              &nbsp; Read more
-            </a>
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div
-          class="card-body"
-          style="background: linear-gradient(139deg, rgba(186, 215, 0, 0.247) 0%, rgba(94, 192, 205, 0.384) 100%)"
-        >
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a
-              class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-              data-bs-toggle="modal"
-              data-bs-target="#productModal"
-            >
-              <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                <i class="fa fa-arrow-right"></i>
-              </span>
-              &nbsp; Read more
-            </a>
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow text-bg-light">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a
-              class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-              data-bs-toggle="modal"
-              data-bs-target="#productModal"
-            >
-              <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                <i class="fa fa-arrow-right"></i>
-              </span>
-              &nbsp; Read more
-            </a>
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow text-bg-light">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a
-              class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-              data-bs-toggle="modal"
-              data-bs-target="#productModal"
-            >
-              <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                <i class="fa fa-arrow-right"></i>
-              </span>
-              &nbsp; Read more
-            </a>
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow text-bg-light">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a
-              class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-              data-bs-toggle="modal"
-              data-bs-target="#productModal"
-            >
-              <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                <i class="fa fa-arrow-right"></i>
-              </span>
-              &nbsp; Read more
-            </a>
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow text-bg-light">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a
-              class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-              data-bs-toggle="modal"
-              data-bs-target="#productModal"
-            >
-              <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                <i class="fa fa-arrow-right"></i>
-              </span>
-              &nbsp; Read more
-            </a>
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow text-bg-light">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a data-fslightbox href="#product" class="card-link stretched-link" data-class="lightbox-on"
-              >&nbsp; Read more</a
-            >
-            <!--  <a
-                  class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-                  data-bs-toggle="modal"
-                  data-bs-target="#productModal">
-                  <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                    <i class="fa fa-arrow-right"></i>
-                  </span>
-                  &nbsp; Read more
-                </a> -->
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6 col-md-4 wow animate__fadeInUp">
-      <div class="card shadow text-bg-light">
-        <img class="card-img-top" src="/sample_med.jpg" />
-        <div class="card-body">
-          <h5 class="card-title">Gabadron-Plus</h5>
-          <h6 class="text-body-secondary">Pregabaline 75 mg, Nortriptyline Hydrochloride 10mg</h6>
-          <p class="card-text text-body-secondary small">
-            Neuropathic Pain Associated with Spondylitis, Arthritis, Sciatica Diabetic Neuropathy, Peripherial
-            Neuropathy, Drug Induced Neuropathy
-          </p>
-          <div class="mt-4 d-flex align-items-center">
-            <!-- <div class=""><a href="#" class="fw-semibold text-dark text-decoration-none stretched-link">Read more ></a></div> -->
-            <a data-fslightbox href="#product" class="card-link stretched-link" data-class="lightbox-on"
-              >&nbsp; Read more</a
-            >
-
-            <!--  <a
-                  class="btn stretched-link btn-outline-success p-0 pe-2 rounded-2 text-nowrap"
-                  data-bs-toggle="modal"
-                  data-bs-target="#productModal">
-                  <span class="d-inline-block position-relative start-0 bg-black bg-opacity-25 rounded-start-2 py-1 px-2">
-                    <i class="fa fa-arrow-right"></i>
-                  </span>
-                  &nbsp; Read more
-                </a> -->
-            <div class="ms-auto text-end">
-              <span class="badge badge-lg bg-warning">New</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </div>
