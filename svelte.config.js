78c3bbe1112dbdc89e191f8df909845c4a1eeba9
Chetan Shinde <14790035+shindecs21@users.noreply.github.com>
2025-01-01 12:55:26 +0530

    404 fix

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
       // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: "404.html",
+      fallback: null,
       precompress: false,
       strict: true,
     }),
