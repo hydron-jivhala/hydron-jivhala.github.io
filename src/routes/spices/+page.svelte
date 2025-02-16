dddac2bf6fa069cc25c6164b8b2c5791bc431b1d
Chetan Shinde <chetan.shinde@yardi.com>
2025-02-16 19:36:09 +0530

    modal for spices

--- src/routes/spices/+page.svelte
+++ src/routes/spices/+page.svelte
@@ -1,11 +1,20 @@
 <script>
   import { onMount } from "svelte";
   import * as main from "../../lib/main.js";
+
   onMount(() => {
     new WOW({ live: false, animateClass: "animate__animated" }).init();
     main.setupStickyNavBar(true);
+
+    const myModal = document.getElementById("spiceModal");
+    myModal.addEventListener("show.bs.modal", function (event) {
+      const card = event.relatedTarget; // Button that triggered the modal
+      const spice = card.getAttribute("data-bs-item"); // Extract info from data-bs-* attributes
+      prod = prodList.find((e) => e.Image == spice);
+    });
   });
 
+  let prod = {};
   let prodList = [
     {
       Name: "Kanda Lasun Masala (Onion Garlic)",
@@ -81,70 +90,70 @@
   ];
 </script>
 
-<div class="container-fluid m-0 pb-5 px-0"> 
-<header class="header-spices py-6 shadow-lg">
-  <div class="container">
-    <div class="row">
-      <img class="card-img-bottom img-fluid w-75 h-75 mx-auto" src="./j_logo2.png" />
-    </div>
+<div class="container-fluid m-0 pb-5 px-0">
+  <header class="header-spices py-6 shadow-lg">
+    <div class="container">
+      <div class="row">
+        <img class="card-img-bottom img-fluid w-75 h-75 mx-auto" src="./j_logo2.png" />
+      </div>
 
-    <div class="row pt-2 border-2 rounded-3 text-center">
-      <h1 class="display-1 my-3 fw-bolder p-2">Jivhala Masale</h1>
-      <h4 class="display-4 my-4 fw-bolder fst-italic text-warning">Taste of Love</h4>
+      <div class="row pt-2 border-2 rounded-3 text-center">
+        <h1 class="display-1 my-3 fw-bolder p-2">Jivhala Masale</h1>
+        <h4 class="display-4 my-4 fw-bolder fst-italic text-warning">Taste of Love</h4>
 
-      <p class="display-6 px-md-8 fw-semibold text-white">
-        At Jivhala, we believe that life is too short for bland food. Our journey began in the heart of spice-loving
-        kitchens, where we discovered the magic that a pinch of the right spice can bring to any dish.
-      </p>
+        <p class="display-6 px-md-8 fw-semibold text-white">
+          At Jivhala, we believe that life is too short for bland food. Our journey began in the heart of spice-loving
+          kitchens, where we discovered the magic that a pinch of the right spice can bring to any dish.
+        </p>
 
-      <div class="features my-4">
-        <div
-          class="mt-1 card shadow-lg bg-dark bg-opacity-50 rounded-4 wow animate__fadeInLeftBig"
-          data-wow-delay=".1s"
-        >
-          <div class="card-body d-flex">
-            <i class="fa fa-wheat-awn fa-4x text-success align-content-center"></i>
-            <div class="px-2 px-sm-4 text-start">
-              <p class="lead fw-normal text-warning">
-                <b>Sourced with Love :</b> We partner directly with farmers who nurture their spice crops like family. Quality
-                and sustainability are our guiding stars.
-              </p>
+        <div class="features my-4">
+          <div
+            class="mt-1 card shadow-lg bg-dark bg-opacity-50 rounded-4 wow animate__fadeInLeftBig"
+            data-wow-delay=".1s"
+          >
+            <div class="card-body d-flex">
+              <i class="fa fa-wheat-awn fa-4x text-success align-content-center"></i>
+              <div class="px-2 px-sm-4 text-start">
+                <p class="lead fw-normal text-warning">
+                  <b>Sourced with Love :</b> We partner directly with farmers who nurture their spice crops like family.
+                  Quality and sustainability are our guiding stars.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          class="mt-1 card shadow-lg bg-dark bg-opacity-50 rounded-4 wow animate__fadeInLeftBig"
-          data-wow-delay=".2s"
-        >
-          <div class="card-body d-flex">
-            <i class="fa fa-pepper-hot fa-4x text-danger align-content-center"></i>
-            <div class="px-2 px-sm-4 text-start">
-              <p class="lead fw-normal text-warning">
-                <b>Freshness Unleashed :</b> Our spices are ground in small batches to preserve their essence. When you open
-                that jar, you’re inhaling pure magic.
-              </p>
+          <div
+            class="mt-1 card shadow-lg bg-dark bg-opacity-50 rounded-4 wow animate__fadeInLeftBig"
+            data-wow-delay=".2s"
+          >
+            <div class="card-body d-flex">
+              <i class="fa fa-pepper-hot fa-4x text-danger align-content-center"></i>
+              <div class="px-2 px-sm-4 text-start">
+                <p class="lead fw-normal text-warning">
+                  <b>Freshness Unleashed :</b> Our spices are ground in small batches to preserve their essence. When you
+                  open that jar, you’re inhaling pure magic.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          class="mt-1 card shadow-lg bg-dark bg-opacity-50 rounded-4 wow animate__fadeInLeftBig"
-          data-wow-delay=".3s"
-        >
-          <div class="card-body d-flex">
-            <i class="fa fa-medal fa-4x text-warning align-content-center"></i>
-            <div class="px-2 px-sm-4 text-danger">
-              <p class="lead fw-normal text-warning">
-                <b>Quality Assurance :</b> Safety and quality are non-negotiable. Our state-of-the-art manufacturing facilities
-                adhere to the highest industry standards. Every batch undergoes rigorous quality control to ensure efficacy
-                and purity.
-              </p>
+          <div
+            class="mt-1 card shadow-lg bg-dark bg-opacity-50 rounded-4 wow animate__fadeInLeftBig"
+            data-wow-delay=".3s"
+          >
+            <div class="card-body d-flex">
+              <i class="fa fa-medal fa-4x text-warning align-content-center"></i>
+              <div class="px-2 px-sm-4 text-danger">
+                <p class="lead fw-normal text-warning">
+                  <b>Quality Assurance :</b> Safety and quality are non-negotiable. Our state-of-the-art manufacturing facilities
+                  adhere to the highest industry standards. Every batch undergoes rigorous quality control to ensure efficacy
+                  and purity.
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</header>
+  </header>
 
   <div class="container-xl mt-6">
     <div class="row g-4 g-lg-5 p-2">
@@ -159,8 +168,15 @@
               <h4 class="card-title text-warning fw-bold">{med.Name}</h4>
 
               <div class="mt-4 d-flex align-items-center">
-                <p class="">{med.Desc}</p>
+                <p class="text-danger-emphasis">{med.Desc}</p>
               </div>
+              <a
+                class="btn stretched-link text-nowrap m-0 p-0"
+                data-bs-toggle="modal"
+                data-bs-target="#spiceModal"
+                data-bs-item={med.Image}
+                >&nbsp;
+              </a>
             </div>
           </div>
         </div>
@@ -169,8 +185,41 @@
   </div>
 </div>
 
+<div
+  class="modal fade custom-modal"
+  id="spiceModal"
+  tabindex="-1"
+  aria-labelledby="exampleModalLabel"
+  aria-hidden="true"
+>
+  <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header py-1">
+        <h2 class="text-warning fw-bold">{prod.Name}</h2>
+        <a class="ms-auto px-2 text-black-50" href="#" data-bs-dismiss="modal"><i class="fa fa-close fa-2x"></i></a>
+      </div>
+
+      <div class="modal-body p-0">
+        <div class="image-container" style="background-color: #5b5000;">
+          <div class="align-self-center text-center">
+            <img src="./{prod.Image}.jpg" alt="" class="img-fluid px-5 px-sm-6 py-2 modal-image" />
+          </div>
+        </div>
+        <div class="mt-4 d-flex align-items-center">
+          <p class="text-center text-danger-emphasis mx-auto">{prod.Desc}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <style>
   .container-fluid {
     background: linear-gradient(270deg, rgb(185 156 137 / 30%) 0%, rgb(253 186 100 / 58%) 100%) !important;
+  }
+
+  .custom-modal .modal-image {
+    max-height: 80vh; /* Adjust this value as needed */
+    width: auto;
   }
 </style>
